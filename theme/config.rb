18e35a9ccb2263b9b2cfec7b91eb3eee4110e993
Josh Corich <joshc@catalyst.net.nz>
2015-03-31 09:22:48 +1300

    Original commit

---
+++ theme/config.rb
@@ -0,0 +1,30 @@
+require 'compass/import-once/activate'
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+
+http_path = "/themes/dms"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+relative_assets = false
+
+line_comments = false
+
+preferred_syntax = :scss
+
+css_dir = "css" # by Compass.app 
+sass_dir = "sass" # by Compass.app 
+images_dir = "images" # by Compass.app 
+output_style = :compact # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = false # by Compass.app 
